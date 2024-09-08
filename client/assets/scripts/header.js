55ce6e41e675a90d6f1f9e0d29817b645936bac8
frekkanzer2 <abatefrancesco98@gmail.com>
2024-09-08 13:47:09 +0200

    Client folder creation

--- client/assets/scripts/header.js
+++ client/assets/scripts/header.js
@@ -6,16 +6,20 @@ document.addEventListener("DOMContentLoaded", function() {
     <div class="top">
 
             <div id="logo">
-                <span class="image avatar48"><img src="images/avatar.png" alt="" /></span>
-                <h1 id="title">HorizonGram</h1>
+                <a href="index.html" id="header-link">
+                    <span class="image avatar48">
+                        <img src="images/avatar.png" alt="" />
+                    </span>
+                    <h1 id="title">HorizonGram</h1>
+                </a>
                 <p>Cloud storage manager</p>
             </div>
 
             <nav id="nav">
                 <ul>
                     <li><a href="" id="top-link"><span class="icon solid fa-upload">Upload a file</span></a></li>
-                    <li><a href="" id="top-link"><span class="icon solid fa-file">Search a file</span></a></li>
-                    <li><a href="" id="top-link"><span class="icon solid fa-folder">New folder</span></a></li>
+                    <li><a href="newfolder.html" id="top-link"><span class="icon solid fa-folder">New folder</span></a></li>
+                    <li><a href="" id="top-link"><span class="icon solid fa-file">Your files</span></a></li>
                 </ul>
             </nav>
 
