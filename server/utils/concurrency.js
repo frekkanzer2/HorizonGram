7b1462675ebcf3f6287c196ddca8710343d328bb
Francesco Abate <abatefrancesco98@gmail.com>
2024-11-28 17:44:47 +0100

    Removed logs from concurrency limit

--- server/utils/concurrency.js
+++ server/utils/concurrency.js
@@ -1,15 +1,12 @@
 exports.limitedConcurrency = async (tasks, limit) => {
     const results = [];
     const executing = [];
-    let index = 1;
     for (const task of tasks) {
         const p = task();
         results.push(p);
         if (limit <= tasks.length) {
             const e = p.finally(() => executing.splice(executing.indexOf(e), 1));
             executing.push(e);
-            console.log(`EXE > Executing task ${index} of ${tasks.length}`);
-            index++;
             if (executing.length >= limit) {
                 await Promise.race(executing);
             }
