c5d86f544a99cb07987809c59dccdff0fb678ce3
frekkanzer2 <abatefrancesco98@gmail.com>
2024-12-12 10:45:36 +0100

    Refactoring + removed sources file from git

--- backupper/backupper.py
+++ backupper/backupper.py
@@ -6,18 +6,22 @@ async def main():
     try:
         await check_server_status()
         print("LOG > Successfully connected to HorizonGram")
+        sources = read_backup_sources()
+        if len(sources) == 0:
+            print("LOG > No folders found into sources file, so there's nothing to backup")
+            return
+        aliases = get_sources_foldernames(sources)
+        print("LOG > The following folders will be backupped on HorizonGram")
+        [print(f">> {source}") for source in sources]
+        print("LOG > The obtained sources will be saved on HorizonGram with the following aliases")
+        [print(f">> {alias}") for alias in aliases]
+    except FileNotFoundError as fe:
+        print(fe)
+        print("ERR > Follow the setup guide for further informations")
+        return
     except Exception as e:
         print(e)
         return
-    sources = read_backup_sources()
-    if len(sources) == 0:
-        print("LOG > No folders found into sources file, so there's nothing to backup")
-        return
-    aliases = get_sources_foldernames(sources)
-    print("LOG > The following folders will be backupped on HorizonGram")
-    [print(f">> {source}") for source in sources]
-    print("LOG > The obtained sources will be saved on HorizonGram with the following aliases")
-    [print(f">> {alias}") for alias in aliases]
 
 if __name__ == "__main__":
     asyncio.run(main())
